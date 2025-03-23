bd87270c3d23d2a057691c291b3169a18037e28f
Rasmus Buurman <rasmus@rbuurman.de>
2025-03-23 03:49:23 +0100

    Implement printing expressions

--- ast_printer.c
+++ ast_printer.c
@@ -4,11 +4,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* forward declarations */
+void _PF_print_expr(PF_Expr *expr);
+
 void _print_ident_list(PF_IdentList *idents) {
 	if (!idents) return;
 	printf("%s", idents->ident);
 	if (idents->tail) printf(", ");
 	_print_ident_list(idents->tail);
+}
+
+void _print_sexp_inner(PF_ExprList *sexp) {
+	if (!sexp) return;
+	_PF_print_expr(sexp->expr);
+	if (sexp->tail) printf(" ");
+	_print_sexp_inner(sexp->tail);
+}
+
+void _print_sexp(PF_ExprList *sexp) {
+	printf("(");
+	_print_sexp_inner(sexp);
+	printf(")");
+}
+
+void _PF_print_expr(PF_Expr *expr) {
+	if (!expr) {
+		printf("ERROR: Expr is NULL\n");
+		exit(1);
+	}
+
+	switch (expr->kind) {
+		case PF_EXPR_ZERO: printf("0"); break;
+		case PF_EXPR_VAR: printf("%s", expr->var); break;
+		case PF_EXPR_SEXP: _print_sexp(expr->sexp); break;
+	}
+}
+
+void PF_print_expr(PF_Expr *expr) {
+	_PF_print_expr(expr);
+	printf("\n");
 }
 
 void print_axiom(PF_Axiom *axiom) {
@@ -20,12 +54,17 @@ void print_axiom(PF_Axiom *axiom) {
 	printf("AXIOM %s", axiom->name);
 	if (axiom->params) printf(" WITH ");
 	_print_ident_list(axiom->params);
+	printf(": ");
+	_PF_print_expr(axiom->lhs);
+	printf(" = ");
+	_PF_print_expr(axiom->rhs);
 	printf("\n");
 }
 
 void print_axioms(PF_AxiomList *axioms) {
 	if (!axioms) return;
 	print_axiom(axioms->axiom);
+	if (axioms->tail) printf("\n");
 	print_axioms(axioms->tail);
 }
 
--- ast_printer.h
+++ ast_printer.h
@@ -4,5 +4,6 @@
 #include "ast.h"
 
 void PF_print_program(PF_Program *prog);
+void PF_print_expr(PF_Expr *expr);
 
 #endif // !AST_PRINTER_H
