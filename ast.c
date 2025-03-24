5df67a8a7fe45923c2a22574b3ac1482ebf3f397
Rasmus Buurman <rasmus@rbuurman.de>
2025-03-24 04:09:49 +0100

    Add PF_Theorem and PF_Proof types

--- ast.c
+++ ast.c
@@ -2,9 +2,10 @@
 
 #include <stdlib.h>
 
-PF_Program *PF_program(PF_AxiomList *axioms) {
+PF_Program *PF_program(PF_AxiomList *axioms, PF_TheoremList *theorems) {
 	PF_Program *prog = malloc(sizeof(PF_Program));
 	prog->axioms = axioms;
+	prog->theorems = theorems;
 	return prog;
 }
 
@@ -29,6 +30,23 @@ PF_AxiomList *PF_axiom_list(PF_Axiom *axiom, PF_AxiomList *tail) {
 	axioms->head = axiom;
 	axioms->tail = tail;
 	return axioms;
+}
+
+PF_Theorem *PF_theorem(PF_Ident name, PF_IdentList *params, PF_Expr *lhs, PF_Expr *rhs, PF_Proof *proof) {
+	PF_Theorem *theorem = malloc(sizeof(PF_Theorem));
+	theorem->name = name;
+	theorem->params = params;
+	theorem->lhs = lhs;
+	theorem->rhs = rhs;
+	theorem->proof = proof;
+	return theorem;
+}
+
+PF_TheoremList *PF_theorem_list(PF_Theorem *theorem, PF_TheoremList *tail) {
+	PF_TheoremList *theorems = malloc(sizeof(PF_TheoremList));
+	theorems->head = theorem;
+	theorems->tail = tail;
+	return theorems;
 }
 
 PF_Expr *PF_expr_num(int num) {
@@ -63,4 +81,27 @@ PF_ExprList *PF_expr_list(PF_Expr *expr, PF_ExprList *tail) {
 	exprs->head = expr;
 	exprs->tail = tail;
 	return exprs;
+}
+
+PF_Proof *PF_proof_direct(PF_Expr *start, PF_ProofNodeTransform *transform) {
+	PF_Proof *proof = malloc(sizeof(PF_Proof));
+	proof->kind = PF_PROOF_DIRECT;
+	proof->direct.start = start;
+	proof->direct.transform = transform;
+	return proof;
+}
+
+PF_ProofNodeExpr *PF_proof_node_expr(PF_Expr *expr, PF_ProofNodeTransform *transform) {
+	PF_ProofNodeExpr *node = malloc(sizeof(PF_ProofNodeExpr));
+	node->expr = expr;
+	node->transform = transform;
+	return node;
+}
+
+PF_ProofNodeTransform *PF_proof_node_transform(PF_Ident name, bool reversed, PF_ProofNodeExpr *expr) {
+	PF_ProofNodeTransform *node = malloc(sizeof(PF_ProofNodeTransform));
+	node->name = name;
+	node->reversed = reversed;
+	node->expr = expr;
+	return node;
 }
--- ast.h
+++ ast.h
@@ -13,11 +13,17 @@ typedef struct _PF_Program PF_Program;
 typedef struct _PF_IdentList PF_IdentList;
 typedef struct _PF_Axiom PF_Axiom;
 typedef struct _PF_AxiomList PF_AxiomList;
+typedef struct _PF_Theorem PF_Theorem;
+typedef struct _PF_TheoremList PF_TheoremList;
 typedef struct _PF_Expr PF_Expr;
 typedef struct _PF_ExprList PF_ExprList;
+typedef struct _PF_Proof PF_Proof;
+typedef struct _PF_ProofNodeExpr PF_ProofNodeExpr;
+typedef struct _PF_ProofNodeTransform PF_ProofNodeTransform;
 
 struct _PF_Program {
 	PF_AxiomList *axioms;
+	PF_TheoremList *theorems;
 };
 
 struct _PF_IdentList {
@@ -33,6 +39,15 @@ struct _PF_Axiom {
 };
 GENERATE_LIST_TYPE(PF_Axiom);
 
+struct _PF_Theorem {
+	PF_Ident name;
+	PF_IdentList *params;
+	PF_Expr *lhs;
+	PF_Expr *rhs;
+	PF_Proof *proof;
+};
+GENERATE_LIST_TYPE(PF_Theorem);
+
 struct _PF_Expr {
 	enum {
 		PF_EXPR_ZERO,
@@ -46,13 +61,41 @@ struct _PF_Expr {
 };
 GENERATE_LIST_TYPE(PF_Expr);
 
-PF_Program *PF_program(PF_AxiomList *axioms);
+struct _PF_Proof {
+	enum {
+		PF_PROOF_DIRECT,
+	} kind;
+	union {
+		struct {
+			PF_Expr *start;
+			PF_ProofNodeTransform *transform;
+		} direct;
+	};
+};
+
+struct _PF_ProofNodeExpr {
+	PF_Expr *expr;
+	PF_ProofNodeTransform *transform;
+};
+
+struct _PF_ProofNodeTransform {
+	PF_Ident name;
+	bool reversed;
+	PF_ProofNodeExpr *expr;
+};
+
+PF_Program *PF_program(PF_AxiomList *axioms, PF_TheoremList *theorems);
 PF_IdentList *PF_ident_list(PF_Ident ident, PF_IdentList *tail);
 PF_Axiom *PF_axiom(PF_Ident name, PF_IdentList *params, PF_Expr *lhs, PF_Expr *rhs);
 PF_AxiomList *PF_axiom_list(PF_Axiom *axiom, PF_AxiomList *tail);
+PF_Theorem *PF_theorem(PF_Ident name, PF_IdentList *params, PF_Expr *lhs, PF_Expr *rhs, PF_Proof *proof);
+PF_TheoremList *PF_theorem_list(PF_Theorem *theorem, PF_TheoremList *tail);
 PF_Expr *PF_expr_num(int num);
 PF_Expr *PF_expr_var(PF_Ident var);
 PF_Expr *PF_expr_sexp(PF_ExprList *sexp);
 PF_ExprList *PF_expr_list(PF_Expr *expr, PF_ExprList *tail);
+PF_Proof *PF_proof_direct(PF_Expr *start, PF_ProofNodeTransform *transform);
+PF_ProofNodeExpr *PF_proof_node_expr(PF_Expr *expr, PF_ProofNodeTransform *transform);
+PF_ProofNodeTransform *PF_proof_node_transform(PF_Ident name, bool reversed, PF_ProofNodeExpr *expr);
 
 #endif // !AST_H
